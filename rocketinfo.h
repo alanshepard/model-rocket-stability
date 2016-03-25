b70f4248fd82f1ce3ecc7ae9c5b4c1c3edc8dc1c
bernardo <b.b.monteiro@gmail.com>
2016-03-25 16:44:44 -0300

    modified:   rocketinfo.h

--- rocketinfo.h
+++ rocketinfo.h
@@ -6,20 +6,27 @@
  * This file is released under the GPLv2
  */
 
-
-/*List of symbols: 
- * M0: rocket's mass without fins;
- * CM0: rocket's center of mass without fins. Origin is on the tip of the nose;
- * CN0: rocket's normal force coefficient without fins;
- * Z0: rocket's center of pressure without fins;
- * diameter: rocket's diameter;
- * length: rocket's length;
- * t: fin's profile maximum thickness in relation to the chord;
- * aspectRatio: desired aspect ratio for the fins.
- */
-
+#ifndef __ROCKETINFO_H
+#define __ROCKETINFO_H
 
 #include <stdio.h>
-enum field {M0,CM0,CN0,Z0,DIAMETER,LENGTH};
+#include <stdlib.h>
 
+#define MAX_COMPONENTS 10
+#define MAX_NAME 10
 
+typedef struct {
+	int ncomponents;
+	double length;
+	double diameter;
+	char* description[MAX_COMPONENTS];
+	double m[MAX_COMPONENTS]; /*mass of each component*/
+	double CM[MAX_COMPONENTS]; /*center of mass of each component*/
+	double CN_alpha[MAX_COMPONENTS]; /*CN_alpha of each component*/
+	double Z[MAX_COMPONENTS]; /*center of pressure of each component*/
+}Rocket;
+
+Rocket* getRocket (FILE* rocketFile);
+void printRocket(const Rocket* rocket, FILE* rocketFile);
+
+#endif //__ROCKETINFO_H
